ccc09b8da4263d9358447c2f468e3ad35f8866ee
github_ajacin <arunjacob2@gmail.com>
2020-07-15 09:01:19 +0530

    Extracted widgets for readability. No functional updates

--- lib/widgets/Settings.dart
+++ lib/widgets/Settings.dart
@@ -5,14 +5,13 @@ import 'package:clearnursing/screens/pages/about-developer.dart';
 import 'package:clearnursing/widgets/messaging-widget.dart';
 import 'package:package_info/package_info.dart';
 
-class Settings extends StatelessWidget {
-  static final String path = "lib/src/pages/settings/settings2.dart";
-  // final TextStyle whiteText = TextStyle(
-  //   color: Theme.of(context).primaryColor,
-  // );
-  // final TextStyle greyTExt = TextStyle(
-  //   color: Theme.of(context).accentColor,
-  // );
+class Settings extends StatefulWidget {
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
   Future<String> getVersionNumber() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
@@ -42,125 +41,17 @@ class Settings extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 const SizedBox(height: 30.0),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).accentColor,
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: imageUrl != null
-                              ? NetworkImage(
-                                  imageUrl,
-                                )
-                              : Image(
-                                  image: AssetImage(
-                                    'assets/google.png',
-                                  ),
-                                  fit: BoxFit.cover,
-                                ),
-                          fit: BoxFit.cover,
-                        ),
-                        border: Border.all(
-                          color: Theme.of(context).highlightColor,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            name,
-                            style: TextStyle(
-                              color: Theme.of(context).accentColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                UserProfileInfo(),
                 const SizedBox(height: 20.0),
-                ListTile(
-                  title: Text("Theme"),
-                  subtitle: Text(
-                    "System Default",
-                    style: TextStyle(
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  trailing: Icon(
-                    Icons.keyboard_arrow_right,
-                    color: Theme.of(context).accentColor,
-                  ),
-                  onTap: () {
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                        'Theme automatically changes according to your device\'s dark mode settings.',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      duration: const Duration(seconds: 3),
-                      backgroundColor: Theme.of(context).highlightColor,
-                    ));
-                  },
-                ),
-                ListTile(
-                  title: Text("Profile Category"),
-                  subtitle: Text(
-                    "Learner",
-                    style: TextStyle(
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  trailing: Icon(
-                    Icons.keyboard_arrow_right,
-                    color: Theme.of(context).accentColor,
-                  ),
-                  onTap: () {},
-                ),
-                SwitchListTile(
-                  title: Text("Notify updates"),
-                  subtitle: Text(
-                    "On",
-                    style: TextStyle(
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  value: true,
-                  onChanged: (val) {},
-                ),
-                SwitchListTile(
-                  title: Text("Practice reminder"),
-                  subtitle: Text(
-                    "Off",
-                    style: TextStyle(
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  value: false,
-                  onChanged: (val) {},
-                ),
+                ThemeInfo(),
+                ProfileCategoryInfo(),
+                PracticeRemiderPreference(),
                 ListTile(
                   title: Text("Application Version"),
                   subtitle: FutureBuilder(
                       future:
                           getVersionNumber(), // The async function we wrote earlier that will be providing the data i.e vers. no
-                      builder: (BuildContext context,
-                              AsyncSnapshot<String> snapshot) =>
-                          Text(
-                            snapshot.hasData
-                                ? snapshot.data
-                                : "Loading version",
-                          ) // The widget using the data
+                      builder: getApplicationVersion // The widget using the data
                       ),
                   trailing: Icon(
                     Icons.system_update_alt,
@@ -168,52 +59,217 @@ class Settings extends StatelessWidget {
                   ),
                   onTap: () {},
                 ),
-                ListTile(
-                  title: Text("About"),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AboutDeveloper()),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: Text("Logout"),
-                  onTap: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Logout"),
-                            content: Text("Are you sure you want to log out?"),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text('Logout'),
-                                onPressed: () {
-                                  signOutGoogle();
-                                  Navigator.of(context).pushAndRemoveUntil(
-                                      MaterialPageRoute(builder: (context) {
-                                    return LoginPage();
-                                  }), ModalRoute.withName('/'));
-                                },
-                              ),
-                              FlatButton(
-                                child: Text('Cancel'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                ),
+                AboutAppInfo(),
+                LogoutTile(),
                 SizedBox(height: 200, child: MessagingWidget())
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget getApplicationVersion(BuildContext context,
+                            AsyncSnapshot<String> snapshot) =>
+                        Text(
+                          snapshot.hasData
+                              ? snapshot.data
+                              : "Loading version",
+                        );
+}
+
+class LogoutTile extends StatelessWidget {
+  const LogoutTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Logout"),
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Logout"),
+                content: Text("Are you sure you want to log out?"),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Logout'),
+                    onPressed: () {
+                      signOutGoogle();
+                      Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(builder: (context) {
+                        return LoginPage();
+                      }), ModalRoute.withName('/'));
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            });
+      },
+    );
+  }
+}
+
+class AboutAppInfo extends StatelessWidget {
+  const AboutAppInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("About"),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AboutDeveloper()),
+        );
+      },
+    );
+  }
+}
+
+class PracticeRemiderPreference extends StatelessWidget {
+  const PracticeRemiderPreference({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      title: Text("Practice reminder"),
+      subtitle: Text(
+        "Off",
+        style: TextStyle(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+      value: false,
+      onChanged: (val) {},
+    );
+  }
+}
+
+class ProfileCategoryInfo extends StatelessWidget {
+  const ProfileCategoryInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Profile Category"),
+      subtitle: Text(
+        "Learner",
+        style: TextStyle(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+      trailing: Icon(
+        Icons.keyboard_arrow_right,
+        color: Theme.of(context).accentColor,
+      ),
+      onTap: () {},
+    );
+  }
+}
+
+class ThemeInfo extends StatelessWidget {
+  const ThemeInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Theme"),
+      subtitle: Text(
+        "System Default",
+        style: TextStyle(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+      trailing: Icon(
+        Icons.keyboard_arrow_right,
+        color: Theme.of(context).accentColor,
+      ),
+      onTap: () {
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text(
+            'Theme automatically changes according to your device\'s dark mode settings.',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          duration: const Duration(seconds: 3),
+          backgroundColor: Theme.of(context).highlightColor,
+        ));
+      },
+    );
+  }
+}
+
+class UserProfileInfo extends StatelessWidget {
+  const UserProfileInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).accentColor,
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: imageUrl != null
+                  ? NetworkImage(
+                      imageUrl,
+                    )
+                  : Image(
+                      image: AssetImage(
+                        'assets/google.png',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+              fit: BoxFit.cover,
+            ),
+            border: Border.all(
+              color: Theme.of(context).highlightColor,
+              width: 2.0,
+            ),
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                name,
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
